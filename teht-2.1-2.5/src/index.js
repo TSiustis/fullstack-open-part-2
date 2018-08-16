35b25654567b2514bd6eb3c166d681d9002d9010
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-16 11:15:33 +0300

    2.1 tehty

--- teht-2.1-2.5/src/index.js
+++ teht-2.1-2.5/src/index.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
+//const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
+const Otsikko = ({otsikko}) => <h1>{otsikko}</h1>
 const Sisalto = (props) => {
-  const [osa1, osa2, osa3] = props.kurssi.osat
+  //const [osa1, osa2, osa3] = {sisalto}
+  //kesken
+  const osat = () => props.sisalto.map(osa => <li key={osa.id}>{osa.nimi} {osa.tehtavia}</li>)
   return(
     <div>
-      <Osa osa={osa1.nimi} tehtavia={osa1.tehtavia} />
-      <Osa osa={osa2.nimi} tehtavia={osa2.tehtavia} />
-      <Osa osa={osa3.nimi} tehtavia={osa3.tehtavia} />
+      <ul>
+      {osat()}
+      </ul>
     </div>
   )
 }
+/*
 const Yhteensa = (props) => {
   const [osa1, osa2, osa3] = props.kurssi.osat
 
   return(
     <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
+  )
+}*/
+
+const Kurssi = (props) => {
+  return (
+    <div>
+    <Otsikko  otsikko= {props.kurssi.nimi}/>
+    <Sisalto sisalto={props.kurssi.osat}/>
+    </div>
   )
 }
 
