90f83687a7229b18bdd6131fcc42c1f82ad0c60e
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2018-09-29 23:32:59 +0300

    2.6 aloitettu
    puhelinluettelo aloitettu

---
+++ puhelinluettelo/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form>
+          <div>
+            nimi: <input />
+          </div>
+
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+
+        </form>
+        <h2>Numerot</h2>
+        ...
+        <div>
+          debug: {this.state.newName}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
