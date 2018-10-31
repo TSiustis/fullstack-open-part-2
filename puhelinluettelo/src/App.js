f5999a5785412ce5c15d501be500992b69427c94
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2018-10-31 14:09:23 +0200

    2.9 tehty
    Lisätty fillteröinti nimen perusteella. Voi käyttää myös vain osaa nimestä

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -7,10 +7,21 @@ class App extends React.Component {
     this.state = {
       persons: [
         { name: 'Arto Hellas',
-          number: "0443386957"}
+          number: "0443386957"},
+        { name: 'Arto Hellas2',
+          number: "0443386957"},
+        { name: 'Arto Järvinen',
+          number: "0443386957"},
+        { name: 'Teemu',
+          number: "0443386957"},
+        { name: 'Koira koiranen',
+          number: "0443386957"},
+        { name: 'Pentti esimerkkis',
+          number: "0443386957"},
       ],
       newName: 'Arto Hellas',
-      newNumber: '0443385732'
+      newNumber: '0443385732',
+      filter:'Arto Hellas'
     }
   }
 
@@ -52,12 +63,26 @@ class App extends React.Component {
     this.setState({ newNumber : event.target.value})
   }
 
+  handlefilterChange = (event) => {
+    this.setState({ filter : event.target.value})
+  }
 
 
   render() {
+    const termLowerCase = this.state.filter.toLowerCase()
+    const found = this.state.persons.filter(person =>{
+      if(person.name.toLowerCase().includes(termLowerCase) ){
+        return person
+      }
+    })
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+        <p>rajaa naytettavia</p>
+        <input value={this.state.filter}
+               onChange={this.handlefilterChange}
+               />
+        <h2>Lisää uusi</h2>
         <form onSubmit= {this.addName}>
           <div>
             nimi: <input value={this.state.newName}
@@ -79,7 +104,7 @@ class App extends React.Component {
 
         <h2>Numerot</h2>
         <div>
-          {this.state.persons.map(item => <Puhelinluettelo key={item.name} name={item.name} number={item.number} />)}
+          {found.map(item => <Puhelinluettelo key={item.name} name={item.name} number={item.number} />)}
         </div>
 
       </div>
